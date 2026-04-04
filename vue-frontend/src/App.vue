d9b78c02f2311acc166b57454ef955f213ddf0f6
cyril <cyrilmel09282001@gmail.com>
2026-04-04 20:38:59 +0800

    Setup of Bootstrap and adding of fundamental

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,47 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1>Vue 3 Composition API Demos</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <CounterDemo />
+    <hr>
+    <ListDemo />
+    <LoginDemo />
+    <InputDemo />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script setup>
+import CounterDemo from './components/CounterDemo.vue'
+import ListDemo from './components/ListDemo.vue'
+import LoginDemo from './components/LoginDemo.vue'
+import InputDemo from './components/InputDemo.vue'
+</script>
---
+++ vue-frontend/src/components/InputDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Input Demo</h2>
+    <input v-model="name" placeholder="Enter your name" />
+    <p>Hello, {{ name }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// reactive state
+const name = ref('')
+</script>
