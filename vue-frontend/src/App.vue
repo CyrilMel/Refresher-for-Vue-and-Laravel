9a6e2cf9ef082c108e3e261d257fd593b8376404
cyril <cyrilmel09282001@gmail.com>
2026-04-06 13:46:48 +0800

    Parent and Child Emit

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -2,11 +2,7 @@
   <div>
     <h1>Vue 3 Composition API Demos</h1>
 
-    <CounterDemo />
-    <hr>
-    <ListDemo />
-    <LoginDemo />
-    <InputDemo />
+    <Parent />
   </div>
 </template>
 
@@ -15,4 +11,5 @@ import CounterDemo from './components/CounterDemo.vue'
 import ListDemo from './components/ListDemo.vue'
 import LoginDemo from './components/LoginDemo.vue'
 import InputDemo from './components/InputDemo.vue'
+import Parent from './components/Parent.vue'
 </script>
---
+++ vue-frontend/src/components/Parent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Emit Example</h1>
+
+    <!-- Display received message -->
+    <p><strong>Message from Child:</strong> {{ receivedMessage }}</p>
+
+    <!-- Child component -->
+    <Child @sendMessage="handleMessage" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Child from './Child.vue';
+// state to store message from child
+const receivedMessage = ref('')
+
+// function to handle emitted data
+function handleMessage(message) {
+  receivedMessage.value = message
+}
+</script>
